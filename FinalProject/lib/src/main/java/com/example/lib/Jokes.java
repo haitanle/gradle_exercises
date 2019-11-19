3541d49f226cfdae73c1ecbc465a5e3e38648922
Tan Le <tanle@Tans-MacBook-Pro.local>
2019-11-19 17:41:26 -0500

    add Admob id and java library for jokes

---
+++ FinalProject/lib/src/main/java/com/example/lib/Jokes.java
@@ -0,0 +1,11 @@
+package com.example.lib;
+
+public class Jokes {
+
+    public Jokes() {
+    }
+
+    public String getJoke(){
+        return "What’s the best thing about Switzerland? I don’t know, but the flag is a big plus.";
+    }
+}
