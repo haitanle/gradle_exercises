00388a6c596483b490055473c3b4efad28952dc7
Tan Le <tanle@Tans-MacBook-Pro.local>
2019-11-20 14:44:03 -0500

    set dependency on joke library for GCE

--- FinalProject/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ FinalProject/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.example.lib.Jokes;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -22,8 +23,11 @@ public class MyEndpoint {
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        //response.setData("Hi, " + name);
 
+        Jokes jokes = new Jokes();
+
+        response.setData("Hi, " + jokes.getJoke());
         return response;
     }
 
